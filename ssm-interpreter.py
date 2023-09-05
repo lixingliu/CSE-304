1ad08fd08e11d1c0b159ee0e05deece8f4eb591f
lixingliu <lixing.liu@stonybrook.edu>
2023-09-05 15:15:49 -0400

    setting up code

--- ssm-interpreter.py
+++ ssm-interpreter.py
@@ -1,2 +1,14 @@
-print("ssm-interpreter")
-print("Hellow World")
+
+
+def ssm_interpreter(file_name):
+    try:
+        f = open(file_name, "r")
+        text = f.read()
+        stack = []
+        
+    except:
+        print("Invalid file path")
+
+file_name = input("Enter path to file: ")
+# file_name = r"C:\Users\lixin\CSE-304\foo.txt"
+ssm_interpreter(file_name)
