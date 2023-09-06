8b41345492fd7278de01d7080b6e84c81ceb8fa5
lixingliu <lixing.liu@stonybrook.edu>
2023-09-05 23:53:08 -0400

    changes

--- ssm-interpreter.py
+++ ssm-interpreter.py
@@ -3,12 +3,74 @@
 def ssm_interpreter(file_name):
     try:
         f = open(file_name, "r")
-        text = f.read()
+        text = f.read().replace("\n", " ").replace(" ", "")
         stack = []
-        
-    except:
-        print("Invalid file path")
+        ildc_command = False
+        word = ""
+        num = ""
+        for character in text:
+            if character.isdigit():
+                num += character
+            else:
+                if num.isdigit():
+                    stack.append(int(num))
+                    num = ""
+                word += character
+                ildc_command = False
+ 
+            match word:
+                case "ildc":
+                    ildc_command = True
+                    word = ""
+                    continue
+                case "iadd":
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    word = ""
+                    stack.append(num1 + num2)
+                case "isub":
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    word = ""
+                    stack.append(num2 - num1)
+                case "imul":
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    word = ""
+                    stack.append(num1 * num2)
+                case "idiv":
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    word = ""
+                    stack.append(num2 / num1)
+                case "imod":
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    word = ""
+                    stack.append(num2 % num1)
+                case "pop":
+                    stack.pop()
+                    word = ""
+                case "dup":
+                    stack.append(stack[-1])
+                    word = ""
+                case "swap":
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    stack.append(num1)
+                    stack.append(num2)
+                    word = ""
+    
+            if ildc_command:                   
+                if not character.isdigit():
+                    print("ildc must be followed by a num!")
+                    return
+                
+        print(stack.pop())
+    except Exception as error:
+        print("errpr: ", error)
 
 file_name = input("Enter path to file: ")
 # file_name = r"C:\Users\lixin\CSE-304\foo.txt"
+# file_name = r"C:\Users\L\CSE-304\foo.txt"
 ssm_interpreter(file_name)
