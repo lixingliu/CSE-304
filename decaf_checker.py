bcc5866f85f1e5e715880502c78e564a226d5c98
lixingliu <lixing.liu@stonybrook.edu>
2023-09-16 19:37:31 -0400

    hw 2 files

--- decaf_checker.py
+++ decaf_checker.py
@@ -1,1 +1,19 @@
-print("testing")
+'''
+Name: Li Xing Liu
+Netid: lixiliu
+Student Id: 113318331
+
+Name: Andy You
+Netid: Andyou
+Student Id: 113494190
+'''
+
+import sys
+
+def decaf_checker():
+    # f = open(sys.argv[1], "r")
+    f = open("./hello_world.decaf", "r")
+    lines = f.readlines()
+    print(lines)
+
+decaf_checker()
