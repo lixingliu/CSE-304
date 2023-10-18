2f23312e7aa5e5e4be0b2082bf4e9fbddc338b30
lixingliu <lixing.liu@stonybrook.edu>
2023-10-18 19:23:23 -0400

    Stuff

---
+++ hw3/decaf_checker.py
@@ -0,0 +1,61 @@
+'''
+Name: Li Xing Liu
+Netid: lixiliu
+Student Id: 113318331
+
+Name: Andy You
+Netid: Andyou
+Student Id: 113494190
+'''
+
+
+
+import sys
+import ply.lex as lex
+import ply.yacc as yacc
+
+def just_scan():
+    # fn = sys.argv[1] if len(sys.argv) > 1 else ""
+    fn = "C:\\Users\\lixin\\CSE-304\\hw2\\hello_world.decaf"
+    if fn == "":
+        print("Missing file name for source program.")
+        print("USAGE: python3 decaf_checker.py <decaf_source_file_name>")
+        sys.exit()
+    import decaf_lexer
+    lexer = lex.lex(module = decaf_lexer, debug = 0)
+
+    fh = open(fn, 'r')
+    source = fh.read()
+    lexer.input(source)
+    next_token = lexer.token()
+    while next_token != None:
+        # print(next_token)
+        next_token = lexer.token()
+# end def just_scan()
+
+
+def main():
+    # fn = sys.argv[1] if len(sys.argv) > 1 else ""
+    fn = "C:\\Users\\lixin\\CSE-304\\hw2\\hello_world.decaf"
+    if fn == "":
+        print("Missing file name for source program.")
+        print("USAGE: python3 decaf_checker.py <decaf_source_file_name>")
+        sys.exit()
+    import decaf_lexer
+    import decaf_parser
+    lexer = lex.lex(module = decaf_lexer, debug = 0)
+    parser = yacc.yacc(module = decaf_parser, debug = 0)
+
+    fh = open(fn, 'r')
+    source = fh.read()
+    fh.close()
+    result = parser.parse(source, lexer = lexer, debug = 0)
+    #print(result)
+    # Parsing Successful
+    #print()
+    print("YES")
+    #print()
+
+if __name__ == "__main__":
+    just_scan()
+    main()
