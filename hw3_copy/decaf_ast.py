30e9080fcce975680642b1c5e26a35e9913a6272
lixingliu <lixing.liu@stonybrook.edu>
2023-11-28 21:19:20 -0500

    .

--- hw3_copy/decaf_ast.py
+++ hw3_copy/decaf_ast.py
@@ -20,38 +20,46 @@ GLOBAL_CONSTRUCTOR_VARIABLE_TABLE = []
 GLOBAL_METHOD_VARIABLE_TABLE = []
 
 
+GLOBAL_CLASS_RECORD = {}
+
 def create_in_class():
+    global CONSTRUCTOR_DICTIONARY, METHOD_DICTIONARY, FIELD_DICTIONARY
+
     pub_sta_mod = Modifier("public", "static")
-    scan_int = Method_Decl("scan_int", pub_sta_mod, Type("int"), Formals_const(), Stmt_List()) #Initializing scan_int
-    scan_float = Method_Decl("scan_float", pub_sta_mod, Type("float"), Formals_const(), Stmt_List()) #Initializing scan_float
+    scan_int = Method_Decl(pub_sta_mod, Type("int"), "scan_int", Formals_const(), Block(Stmt_List())) #Initializing scan_int
+    scan_float = Method_Decl(pub_sta_mod, Type("float"), "scan_int", Formals_const(), Block(Stmt_List())) #Initializing scan_float
     class_in_body_decl = Class_Body_List(scan_int)
     class_in_body_decl.classBodyItems.append(scan_float)
-    class_in = Class_decl("In", "", class_in_body_decl) #Append the two methods to In's class_body_decl
+    class_in = Class_decl("In", class_in_body_decl) #Append the two methods to In's class_body_decl
     return class_in
 
 def create_out_class():
+    global CONSTRUCTOR_DICTIONARY, METHOD_DICTIONARY, FIELD_DICTIONARY
+
     pub_sta_mod = Modifier("public", "static")
     print_int_formal_cont = Formals_const()
     print_int_formal_cont.formals.append(Formal_param(Type("int"), Variable("i")))
-    print_int = Method_Decl("print", pub_sta_mod, None, print_int_formal_cont, Stmt_List()) #Initializing print_int
+    stmt_list = Stmt_List()
+    stmt_list.stmts.append(Return(""))
+    print_int = Method_Decl(pub_sta_mod, Type("int"),"print", print_int_formal_cont, Block(stmt_list)) #Initializing print_int
 
     print_float_formal_cont = Formals_const()
     print_float_formal_cont.formals.append(Formal_param(Type("float"), Variable("f")))
-    print_float = Method_Decl("print", pub_sta_mod, None, print_float_formal_cont, Stmt_List()) #Initializing print_float
+    print_float = Method_Decl(pub_sta_mod, Type('float'), "print", print_float_formal_cont, Block(stmt_list)) #Initializing print_float
 
     print_boolean_formal_cont = Formals_const()
     print_boolean_formal_cont.formals.append(Formal_param(Type("boolean"), Variable("b")))
-    print_boolean = Method_Decl("print", pub_sta_mod, None, print_boolean_formal_cont, Stmt_List()) #Initializing print_boolean
+    print_boolean = Method_Decl(pub_sta_mod, Type("boolean"), "print", print_boolean_formal_cont, Block(stmt_list)) #Initializing print_boolean
 
     print_string_formal_cont = Formals_const()
     print_string_formal_cont.formals.append(Formal_param(Type("string"), Variable("s")))
-    print_string = Method_Decl("print", pub_sta_mod, None, print_string_formal_cont, Stmt_List()) #Initializing print_string
+    print_string = Method_Decl(pub_sta_mod, Type("string"), "print", print_string_formal_cont, Block(stmt_list)) #Initializing print_string
 
     class_out_body_decl = Class_Body_List(print_int)
     class_out_body_decl.classBodyItems.append(print_float)
     class_out_body_decl.classBodyItems.append(print_boolean)
     class_out_body_decl.classBodyItems.append(print_string)
-    class_out = Class_decl("Out", "", class_out_body_decl) #Append the four methods to Out's class_body_decl
+    class_out = Class_decl("Out", class_out_body_decl) #Append the four methods to Out's class_body_decl
     return class_out
 
 class Node():
@@ -70,17 +78,39 @@ class Program(Node):
     def __init__(self, classes):
         super().__init__()
         self.classes = classes
+        create_in_class()
+        create_out_class()
     def __str__(self):
+        global CLASS_NAME, METHOD_DICTIONARY, CONSTRUCTOR_DICTIONARY, GLOBAL_CLASS_RECORD, FIELD_DICTIONARY
         result = ""
-        # self.classes.classList.append(create_in_class())
-        # self.classes.classList.append(create_out_class())
-        for class_object in self.classes.classList[::-1]:
-            if (class_object.className == 'Out' or class_object.className == 'In'):
-                continue
-            global CLASS_NAME
-            CLASS_NAME = class_object.className
-            result = result + (str(class_object) + "\n") + "----------------------\n"
-        return result
+
+
+
+        # for class_object in self.classes.classList[::-1]:
+        #     CLASS_NAME = class_object.className
+        #     result = result + (str(class_object) + "\n") + "----------------------\n"
+        #     if (class_object.className in GLOBAL_CLASS_RECORD.keys()):
+        #         print("we have a problem, why is there 2 classes with the same name")
+        #         sys.exit()
+        #     GLOBAL_CLASS_RECORD[class_object.className] = {
+        #         'constructors': CONSTRUCTOR_DICTIONARY,
+        #         'fields': FIELD_DICTIONARY,
+        #         'methods': METHOD_DICTIONARY,
+        #     }
+        #     METHOD_DICTIONARY = {}
+        #     CONSTRUCTOR_DICTIONARY = {} 
+        #     FIELD_DICTIONARY = {}
+        result = ""
+        for key, value in GLOBAL_CLASS_RECORD.items():
+            # print(key, value)
+            field_result = "Fields:\n"
+            for field_key, field_value in value["fields"].items():
+                field_result += f'FIELD {field_key}, {field_value["variableName"]}, {field_value["className"]}, {field_value["fieldModifier"]}, {field_value["type"]}\n'
+            result = result + f'Class Name: {key}\nSuperclass Name: {value["superClassName"]}\n{field_result}\n' 
+            # print("\n\n")
+        print(result)
+        # return result
+        return ""
     
 class Class_decl_list(Node):
     def __init__(self):
@@ -94,9 +124,25 @@ class Class_decl(Node):
         self.className = className
         self.superClassName = superClassName
         self.classBody = classBody
-        self.superClassName = superClassName
+        
+        global GLOBAL_CLASS_RECORD, CONSTRUCTOR_DICTIONARY, FIELD_DICTIONARY, METHOD_DICTIONARY, CLASS_NAME
+
+        CLASS_NAME = self.className
+
+        GLOBAL_CLASS_RECORD[self.className] = {
+            'className': self.className,
+            'superClassName': self.superClassName,
+            'constructors': CONSTRUCTOR_DICTIONARY,
+            'fields': FIELD_DICTIONARY,
+            'methods': METHOD_DICTIONARY,            
+        } 
+
+        CONSTRUCTOR_DICTIONARY = {}
+        FIELD_DICTIONARY = {}
+        METHOD_DICTIONARY = {}
+
     def __str__(self):            
-        global CONSTRUCTOR_PARAM_KEY, METHOD_PARAM_KEY, GLOBAL_CONSTRUCTOR_VARIABLE_TABLE, GLOBAL_METHOD_VARIABLE_TABLE
+        global CONSTRUCTOR_PARAM_KEY, METHOD_PARAM_KEY, GLOBAL_CONSTRUCTOR_VARIABLE_TABLE, GLOBAL_METHOD_VARIABLE_TABLE, METHOD_DICTIONARY, CONSTRUCTOR_DICTIONARY, FIELD_DICTIONARY
 # ============================================================================================
         field_result = "Fields:\n"
         for key, value in FIELD_DICTIONARY.items():
@@ -115,16 +161,17 @@ class Class_decl(Node):
                     'variableKind': 'formal',
                     'variableType': formal.type
                 }
-                constructorParamsId.append(CONSTRUCTOR_PARAM_KEY)
                 CONSTRUCTOR_PARAM_KEY += 1
             GLOBAL_CONSTRUCTOR_VARIABLE_TABLE.append(variableTable)
             block_string_representation = str(value['block'])
             for variableTable in GLOBAL_CONSTRUCTOR_VARIABLE_TABLE:
                 for key, stuff, in variableTable.items():
                     variable_result = variable_result + f'VARIABLE {key}, {stuff["variableName"]}, {stuff["variableKind"]}, {stuff["variableType"]}\n'
+                    constructorParamsId.append(key)
+
             constructor_result = f'{constructor_result} {str(constructorParamsId)[1:-1]}\n{variable_result}Constructor Body:\n{block_string_representation}\n'
 
-            GLOBAL_CONSTRUCTOR_VARIABLE_TABLE = {}
+            GLOBAL_CONSTRUCTOR_VARIABLE_TABLE = []
 # ============================================================================================
         method_result = "Methods:\n"
         for key, value in METHOD_DICTIONARY.items():
@@ -139,31 +186,24 @@ class Class_decl(Node):
                     'variableKind': 'formal',
                     'variableType': formal.type
                 }
-                methodParamsId.append(METHOD_PARAM_KEY)
                 METHOD_PARAM_KEY += 1
             GLOBAL_METHOD_VARIABLE_TABLE.append(variableTable)
             block_string_representation = str(value['block'])
             for variableTable in GLOBAL_METHOD_VARIABLE_TABLE:
                 for key, stuff in variableTable.items():
                     variable_result = variable_result + f'VARIABLE {key}, {stuff["variableName"]}, {stuff["variableKind"]}, {stuff["variableType"]}\n'
+                    methodParamsId.append(key)
+
             method_result = f'{method_result} {str(methodParamsId)[1:-1]}\n{variable_result}Method Body:\n{block_string_representation}\n'
             GLOBAL_METHOD_VARIABLE_TABLE = []
 # ============================================================================================
+
         return f'Class Name: {self.className}\nSuperclass Name: {self.superClassName}\n{field_result}{constructor_result}{method_result}'
     
 class Class_Body_List(Node):
     def __init__(self, singleItem):
         super().__init__()
         self.classBodyItems = [singleItem]
-    def __str__(self):
-        result = ""
-        for item in self.classBodyItems:
-            if (isinstance(item, Constructor_Decl) and len(CONSTRUCTOR_DICTIONARY) == 0):
-                result = result + "Constructors:\n"
-            if (isinstance(item, Field_Decl)):
-                continue
-            result = result + str(item)
-        return result
     
 class Field_Decl(Node):
     def __init__(self, fieldModifier, fieldVarDecl):
@@ -172,7 +212,7 @@ class Field_Decl(Node):
         self.fieldVarDecl = fieldVarDecl
         global FIELD_KEY, CLASS_NAME, FIELD_DICTIONARY
         
-        result = ''
+        # result = ''
         for variable in self.fieldVarDecl.variable_list.vars[::-1]:
             FIELD_DICTIONARY[FIELD_KEY] = {
                 'className': CLASS_NAME,
@@ -180,33 +220,8 @@ class Field_Decl(Node):
                 'fieldModifier': str(self.fieldModifier),
                 'type': str(self.fieldVarDecl.type)
             }
-            result = result + f'FIELD {FIELD_KEY}, {variable}, {CLASS_NAME}, {str(self.fieldModifier)}, {str(self.fieldVarDecl.type)}\n' 
+            # result = result + f'FIELD {FIELD_KEY}, {variable}, {CLASS_NAME}, {str(self.fieldModifier)}, {str(self.fieldVarDecl.type)}\n' 
             FIELD_KEY += 1
-
-class Modifier(Node):
-    def __init__(self, visibility, applicability = None):
-        super().__init__()
-        self.visibility = visibility
-        self.applicability = applicability
-        if self.visibility == None:
-            self.visibility = "private"
-        if self.applicability == "static":
-            self.applicability = "class"
-        else:
-            self.applicability = "instance"
-    def __str__(self):
-        return f'{str(self.visibility)}, {str(self.applicability)}'
-    
-class Var_Decl(Node):
-    def __init__(self, type, variable_list):
-        super().__init__()
-        self.type = type
-        self.variable_list = variable_list
-    
-class Variables_cont(Node):
-    def __init__(self):
-        super().__init__()
-        self.vars = []
 
 class Constructor_Decl(Node):
     def __init__(self, modifier, constructorName, formals, block):
@@ -248,6 +263,31 @@ class Method_Decl(Node):
             'block': self.block
         }
         METHOD_KEY += 1
+
+class Modifier(Node):
+    def __init__(self, visibility, applicability = None):
+        super().__init__()
+        self.visibility = visibility
+        self.applicability = applicability
+        if self.visibility == None:
+            self.visibility = "private"
+        if self.applicability == "static":
+            self.applicability = "class"
+        else:
+            self.applicability = "instance"
+    def __str__(self):
+        return f'{str(self.visibility)}, {str(self.applicability)}'
+    
+class Var_Decl(Node):
+    def __init__(self, type, variable_list):
+        super().__init__()
+        self.type = type
+        self.variable_list = variable_list
+    
+class Variables_cont(Node):
+    def __init__(self):
+        super().__init__()
+        self.vars = []
     
 class Formals_const(Node):
     def __init__(self):
@@ -314,7 +354,7 @@ class Block(Node):
             elif (isinstance(stmt, Method_Invocation)):
                 result += str(stmt)
             prevStmt = stmt
-        return result + "])"
+        return result + "\n])"
 
 class Stmt_List(Node):
     def __init__(self):
