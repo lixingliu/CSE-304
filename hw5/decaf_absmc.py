1a60a80700a521640fdbdf9030d43f0dde5ab247
AndyYou0115 <90625902+AndyYou0115@users.noreply.github.com>
2023-12-09 22:29:13 -0500

    Test case 2

--- hw5/decaf_absmc.py
+++ hw5/decaf_absmc.py
@@ -5,6 +5,13 @@ heap = []
 control_stack = []
 data_stack = []
 
+cur_reg = 0
+
+def get_next_temp_reg ():
+   cur = cur_reg 
+   cur_reg = cur_reg + 1
+   return cur
+
 def add_temp_reg (value):
     temp_reg.append(value)
 
@@ -19,3 +26,33 @@ def sub (r1, r2, r3):
 
 def add (r1, r2, r3):
     temp_reg[r1] = temp_reg[r2] + temp_reg[r3]
+
+def mul (r1, r2, r3):
+    temp_reg[r1] = temp_reg[r2] * temp_reg[r3]
+
+def div (r1, r2, r3):
+    temp_reg[r1] = temp_reg[r2] / temp_reg[r3]
+
+def and_b (r1, r2, r3):
+   temp_reg[r1] = temp_reg[r2] and temp_reg[r3]
+
+def or_b (r1, r2, r3):
+   temp_reg[r1] = temp_reg[r2] or temp_reg[r3]
+
+def ilt (r1, r2, r3):
+   temp_reg[r1] = temp_reg[r2] < temp_reg[r3]
+
+def ileq (r1, r2, r3):
+   temp_reg[r1] = temp_reg[r2] <= temp_reg[r3]
+
+def igt (r1, r2, r3):
+   temp_reg[r1] = temp_reg[r2] > temp_reg[r3]
+
+def igeq (r1, r2, r3):
+   temp_reg[r1] = temp_reg[r2] >= temp_reg[r3]
+
+def ieq (r1, r2, r3):
+   temp_reg[r1] = temp_reg[r2] == temp_reg[r3]
+
+def ineq (r1, r2, r3):
+   temp_reg[r1] = temp_reg[r2] != temp_reg[r3]
--- hw5/decaf_codegen.py
+++ hw5/decaf_codegen.py
@@ -2,6 +2,7 @@ import decaf_ast
 import decaf_absmc as absmc
 import decaf_typecheck as typechecker
 import sys
+import re
 
 global print 
 print = ""
@@ -13,6 +14,9 @@ def assign (lhs, type, rhs, value):
 
     if ("Integer-constant" in rhs):
         move_immed_i(int(lhs_number)-1, int(value.literal))
+    
+    elif("Float-constant" in rhs):
+        move_immed_f(int(lhs_number)-1, int(value.literal))
 
     elif ("Unary-expression" in rhs):
         if("MINUS" in rhs and "Variable" in rhs):
@@ -29,12 +33,54 @@ def assign (lhs, type, rhs, value):
     elif ("False" in rhs):
         move_immed_i(int(lhs_number)-1, 0)
 
+    elif("Binary("in rhs):
+        if("Variable(" in rhs):
+            regs = re.findall(r'Variable\((\d+)\)', rhs)
+
+            if("add" in rhs):
+                if(type == "int"):
+                    iadd(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+                elif(type == "float"):
+                    fadd(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("mul" in rhs):
+                if(type == "int"):
+                    imul(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+                elif(type == "float"):
+                    fmul(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("sub" in rhs):
+                if(type == "int"):
+                    isub(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+                elif(type == "float"):
+                    fsub(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("div" in rhs):
+                if(type == "int"):
+                    idiv(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+                elif(type =="float"):
+                    fdiv(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("and," in rhs):
+                and_b(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("or," in rhs):
+                or_b(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("lt," in rhs):
+                ilt(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("leq," in rhs):
+                ileq(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("gt," in rhs):
+                igt(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("geq," in rhs):
+                igeq(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("eq," in rhs):
+                ieq(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+            elif("neq," in rhs):
+                ineq(int(lhs_number)-1, int(regs[0])-1, int(regs[1])-1)
+
     elif("Variable(" in rhs):
         start_index = rhs.find("Variable(") + len("Variable(")
         end_index = rhs.find("))", start_index)
         number = rhs[start_index:end_index]
 
         move(int(lhs_number)-1, int(number)-1)
+
     
 def auto (pre, post, lhs):
     lhs_start_index = lhs.find("(Variable(") + len("(Variable(")
@@ -64,11 +110,89 @@ def move (r1, r2):
     print = print + "\nmove " + "t" + str(r1) + ", " + "t" + str(r2)
 
 def isub (r1, r2, r3):
-    absmc.sub(r1, r2, r3)
+    #absmc.sub(r1, r2, r3)
     global print
     print = print + "\nisub " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
 
 def iadd (r1, r2, r3):
-    absmc.add(r1, r2, r3)
+    #absmc.add(r1, r2, r3)
     global print
     print = print + "\niadd " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def imul (r1, r2, r3):
+    #absmc.mul(r1, r2, r3)
+    global print
+    print = print + "\nimul " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def idiv (r1, r2, r3):
+    #absmc.div(r1, r2, r3)
+    global print
+    print = print + "\nidiv " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def move_immed_f (reg, value):
+    absmc.set_temp_reg_i(reg, value)
+    global print
+    if("." in str(value)):
+        print = print + "\nmove_immed_f " + "t" + str(reg) + ", " + str(value)
+    else:
+        print = print + "\nmove_immed_f " + "t" + str(reg) + ", " + str(value) + ".0" 
+
+def fsub (r1, r2, r3):
+    #absmc.sub(r1, r2, r3)
+    global print
+    print = print + "\nfsub " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def fadd (r1, r2, r3):
+    #absmc.add(r1, r2, r3)
+    global print
+    print = print + "\nfadd " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def fmul (r1, r2, r3):
+    #absmc.mul(r1, r2, r3)
+    global print
+    print = print + "\nfmul " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def fdiv (r1, r2, r3):
+    #absmc.div(r1, r2, r3)
+    global print
+    print = print + "\nfdiv " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def and_b (r1, r2, r3):
+    #absmc.and_b(r1, r2, r3)
+    global print
+    print = print + "\nand " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def or_b (r1, r2, r3):
+    #absmc.or_b(r1, r2, r3)
+    global print
+    print = print + "\nor " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def ilt (r1, r2, r3):
+    #absmc.ilt(r1, r2, r3)
+    global print
+    print = print + "\nilt " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3) 
+
+def ileq (r1, r2, r3):
+    #absmc.ileq(r1, r2, r3)
+    global print
+    print = print + "\nileq " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def igt (r1, r2, r3):
+    #absmc.or_b(r1, r2, r3)
+    global print
+    print = print + "\nigt " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def igeq (r1, r2, r3):
+    #absmc.igeq(r1, r2, r3)
+    global print
+    print = print + "\nigeq " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def ieq (r1, r2, r3):
+    #absmc.ieq(r1, r2, r3)
+    global print
+    print = print + "\nieq " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
+
+def ineq (r1, r2, r3):
+    #absmc.ineq(r1, r2, r3)
+    global print
+    print = print + "\nineq " + "t" + str(r1) + ", " + "t" + str(r2) + ", " + "t" + str(r3)
